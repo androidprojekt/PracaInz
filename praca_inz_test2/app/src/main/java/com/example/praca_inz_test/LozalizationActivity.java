170153e589dc34c59969af5f84ff987c689e38cc
Bartosz-Gregorczyk <64013735+Bartosz-Gregorczyk@users.noreply.github.com>
2020-12-30 21:52:17 +0100

    new edit

--- praca_inz_test2/app/src/main/java/com/example/praca_inz_test/LozalizationActivity.java
+++ praca_inz_test2/app/src/main/java/com/example/praca_inz_test/LozalizationActivity.java
@@ -4,6 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.Manifest;
 import android.app.Activity;
@@ -21,10 +26,13 @@ import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
 import android.text.format.Formatter;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -41,156 +49,73 @@ import static java.lang.String.valueOf;
 
 public class LozalizationActivity extends AppCompatActivity {
 
-    //------------------------------------------TYLKO TESTY - DZIALA NIEPOPRAWNIE--------------------------------------
+    NavigationView navigationView;
 
 
-    DatabaseReference reference;
-    TextView placeWhereUAre;    // This TextView describes where you are
-    Button btnCheckRssi;        // When you press this button you receive info about RSSI and place where you are
-    Fingerprint fingerprint;  //object of fingerprint store in database
-    long maxid=0;             // variable used to autoincrement in database
-    BroadcastReceiver wifiReceiver;
-    WifiManager wifiManager;
-    //private ArrayList<Integer> arrayList = new ArrayList<>();
-    private List<ScanResult> results;
-    boolean flag=false;
-    List names;
-    String rssiWifiName1;
-    String rssiWifiName2;
-    String rssiWifiName3;
-    String rssiBle1;
-    String rssiBle2;
-
-    int actualRssiFunbox;
-    int actualRssiAsia_barti;
-    int actualRssiGola;
-
-    int DbRssiFunbox;
-    int DbRssiAsia_barti;
-    int DbRssiGola;
-    String placetemp;
-    String place = "unknown";
-    int min;
-    int euclideanSum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lozalization);
-        overridePendingTransition(R.anim.fade_in, R.anim.fadeout); //animacja przejscia
+        overridePendingTransition(R.anim.fade_in, R.anim.fadeout); //moving activity animation
 
-        final Activity activity = this;
-
-        btnCheckRssi = (Button) findViewById(R.id.checkRssiBtnId);
-        placeWhereUAre= (TextView) findViewById(R.id.placeTextViewId);
-
-        fingerprint = new Fingerprint();
-        reference = FirebaseDatabase.getInstance().getReference().child("Fingerprint");
-        wifiReceiver = new LozalizationActivity().wifiReceiver;
-
-        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-
-        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(activity,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},0);
-        }
-
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists())
-                    maxid= (dataSnapshot.getChildrenCount()); //przydatne do autoinkrementacji id w bazie
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                //procedura w przypadku bledu
-            }
-        });
-
-
-        btnCheckRssi.setOnClickListener(new View.OnClickListener() {
+        final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        findViewById(R.id.imageNavigationMenu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-                wifiManager.startScan(); //rozpoczecie skanowania
-                results = wifiManager.getScanResults(); //przypisanie wynikow skanowania do listy elementow typu ScanResults
-                TextView actualWifi1Tv = findViewById(R.id.actualWifi1RssiId);
-                TextView actualWifi2Tv = findViewById(R.id.actualWifi2RssiId);
-                TextView actualWifi3Tv = findViewById(R.id.actualWifi3RssiId);
-
-                for (int i=0;i<results.size();i++)  //petla po liscie znalezionych sieci WIFI
-                {
-                    if(results.get(i).SSID.contains("FunBox2-5DE4"))
-                    {
-                        actualRssiFunbox= results.get(i).level;
-                        actualWifi1Tv.setText(String.valueOf(results.get(i).level));
-                    }
-
-                    if(results.get(i).SSID.contains("asia"))
-                    {
-                        actualRssiAsia_barti=results.get(i).level;
-
-                        actualWifi2Tv.setText(String.valueOf(results.get(i).level));
-                    }
-
-                    if(results.get(i).SSID.contains("gola"))
-                    {
-                        actualRssiGola = results.get(i).level;
-                        actualWifi3Tv.setText(String.valueOf(results.get(i).level));
-                    }
-
-                }
-
-                reference = FirebaseDatabase.getInstance().getReference().child("Fingerprint");
-                reference.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-            //---------------------deklaracja zmiennych okreslajacych RSSI pobrane z bazy danych oraz RSSI aktualnie pobranych------------
-                names = new ArrayList<String>();
-
-            //------------------------------------------------------------------------------------------------------------------------------
-                    min=1000;
-            //---------------------petla po ustalonych lokalizacjach (typu miejsce przy eksponacie pierwszym, drugim itp.-------------------
-                        for(DataSnapshot dataSnapshot1:dataSnapshot.getChildren()) {
-
-                            placetemp=dataSnapshot1.getKey();
-                            DbRssiFunbox =Integer.parseInt(dataSnapshot1.child("FunBox2-5DE4").child("wifiRssi").getValue().toString());
-                            DbRssiAsia_barti=Integer.parseInt(dataSnapshot1.child("asia_barti").child("wifiRssi").getValue().toString());
-                            DbRssiGola = Integer.parseInt(dataSnapshot1.child("gola").child("wifiRssi").getValue().toString());
-
-                            euclideanSum =  Math.abs(actualRssiAsia_barti-DbRssiAsia_barti)+ Math.abs(actualRssiGola-DbRssiGola)+
-                                    Math.abs(actualRssiFunbox-DbRssiFunbox);
-
-                            if(min>euclideanSum)
-                            {
-                                min=euclideanSum;
-                                place = placetemp;
-                            }
-                            //---------------------skanowanie w poszukiwaniu dostepnych sieci WIFI--------------------
-                        }
-
-
-                        TextView test = findViewById(R.id.baseWifi1RssiId);
-                        test.setText(String.valueOf(min));
-                        TextView placeTv= findViewById(R.id.placeTextViewId);
-                        placeTv.setText(place);
-
-
-                          //--------------------------------------------------------------------------------------------
-
-            //------------------------------------------------------------------------------------------------------------
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                    }
-                });
+                drawerLayout.openDrawer(GravityCompat.START);
             }
         });
-    } //koniec oncreate
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
+
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+                switch(id)
+                {
+                    case R.id.menuExhibition:
+                        goToOptionMenuItem(1);
+                    break;
+                    case R.id.menuInformation:
+                        goToOptionMenuItem(2);
+                        break;
+                    case R.id.menuFavorites:
+                        goToOptionMenuItem(3);
+                        break;
+                    case R.id.menuSettings:
+                        goToOptionMenuItem(4);
+                        break;
+                    case R.id.menuAbout:
+                        goToOptionMenuItem(5);
+                        break;
+                    case R.id.menuLogout:
+                        //goToOptionMenuItem(6);
+                        break;
+                    default:
+                        return true;
+                }
+                return true;
+
+            }
+        });
+
+    }
+
+    public void goToOptionMenuItem(Integer id)
+    {
+        Intent intent ;
+        intent = new Intent(getApplicationContext(),OptionsFromMenuActivity.class);
+        intent.putExtra("OPTION_ID",id);
+        startActivity(intent);
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+
 
     @Override
     protected void onPause() {
@@ -198,10 +123,9 @@ public class LozalizationActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.fade_in, R.anim.fadeout);
     }
 
-    public void goToSavingLayout(View view) {
-        Intent goToSaving;
-        goToSaving = new Intent(getApplicationContext(),SavingActivity.class);
-        startActivity(goToSaving);
-    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
 
+    }
 }
---
+++ praca_inz_test2/app/src/main/java/com/example/praca_inz_test/OptionsFromMenuActivity.java
@@ -0,0 +1,82 @@
+package com.example.praca_inz_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class OptionsFromMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options_from_menu);
+
+        //------------------------------------------------------------------------------------------
+        Intent receivedIntent = getIntent();
+        Integer optionType=receivedIntent.getIntExtra("OPTION_ID",0);
+        //  1--> menuExhibition
+        //  2--> menuInformation
+        //  3--> menuFavorites
+        //  4--> menuSetting
+        //  5--> menuAbout
+        //  6--> menuLogout
+        //------------------------------------------------------------------------------------------
+
+        Fragment fragment=null;
+        switch(optionType)
+        {
+            case 1:
+                fragment = new exhibitionFragment();
+                loadFragment(fragment);
+                break;
+            case 2:
+                fragment = new InformationsFragment();
+                loadFragment(fragment);
+                break;
+            case 3:
+                fragment = new favoritesFragment();
+                loadFragment(fragment);
+                break;
+            case 4:
+                fragment = new settingsFragment();
+                loadFragment(fragment);
+                break;
+            case 5:
+                fragment = new aboutUsFragment();
+                loadFragment(fragment);
+                break;
+            case 6:
+                //logout
+                break;
+            default:
+
+        }
+
+    }
+
+
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment ,fragment).commit();
+        //DrawerLayout drawerLayout = findViewById(R.id.drawerLayout); only works in localization activity
+        //drawerLayout.closeDrawer(GravityCompat.START); only works in localization activity
+        //fragmentTransaction.addToBackStack(null); :-)
+    }
+
+    private void loadFragmentWithStack(Fragment fragment) {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment ,fragment).commit();
+        //DrawerLayout drawerLayout = findViewById(R.id.drawerLayout); only works in localization activity
+        //drawerLayout.closeDrawer(GravityCompat.START); only works in localization activity
+        fragmentTransaction.addToBackStack(null);
+    }
+}
