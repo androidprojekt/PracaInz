b1106863073e9b5b483ad119036b75633aaf1ea0
Bartosz-Gregorczyk <64013735+Bartosz-Gregorczyk@users.noreply.github.com>
2021-01-08 13:56:26 +0100

    EDIT

--- praca_inz_test2/app/src/main/java/com/example/praca_inz_test/instructionActivity.java
+++ praca_inz_test2/app/src/main/java/com/example/praca_inz_test/instructionActivity.java
@@ -14,7 +14,7 @@ public class instructionActivity extends AppCompatActivity {
     private SliderAdapter sliderAdapter;
     private LinearLayout mDotLayout;
     private TextView[] mDots;
-    Button button;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
